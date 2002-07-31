7e203c856d09f6cf7f4786d995108b20a905786d
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2002-07-31 15:00:33 +0000

    Added time stamp to generated index file.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@250 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- xml/html.cmd
+++ xml/html.cmd
@@ -6,13 +6,10 @@ SET XT_PATH=c:\xt
 SET SAX_PATH=c:\xt
 SET XP_PATH=c:\xt
 
-SET XSLT="lgthtml.xsl"
+SET XSLT=lgthtml.xsl
 
-IF !%1==! SET TITLE="Entity documentation index"
-IF NOT !%1==! SET TITLE = %1%
-
-ECHO %1%
-ECHO %TITLE%
+IF "%1" == "" SET INDEX_TITLE=Entity documentation index
+IF NOT "%1" == "" SET INDEX_TITLE=%1%
 
 ECHO This script converts all .xml files in the current directory to .html
 ECHO files applying the XSLT transformation defined in the $XSLT file
@@ -24,9 +21,9 @@ ECHO value as the title of the index.html file.
 ECHO.
 ECHO converting XML files to HTML...
 
-FOR %%f IN (*.xml) IF EXIST %%~nf.html DEL %%~nf.html
+FOR /f "tokens=1-2 delims=." %%f IN ('DIR /b *.xml') DO IF EXIST %%f.html DEL %%f.html
 
-FOR %%f IN (*.xml) DO %JAVA_HOME%\bin\java -cp "%XT_PATH%\xt.jar;%SAX_PATH%\sax.jar;%XP_PATH%\xp.jar" -Dcom.jclark.xsl.sax.parser=com.jclark.xml.sax.CommentDriver com.jclark.xsl.sax.Driver %%f %XSLT% %%~nf.html
+FOR /f "tokens=1-2 delims=." %%f IN ('DIR /b *.xml') DO %JAVA_HOME%\bin\java -cp "%XT_PATH%\xt.jar;%SAX_PATH%\sax.jar;%XP_PATH%\xp.jar" -Dcom.jclark.xsl.sax.parser=com.jclark.xml.sax.CommentDriver com.jclark.xsl.sax.Driver %%f.xml %XSLT% %%f.html
 
 ECHO conversion done
 ECHO.
@@ -37,20 +34,22 @@ IF EXIST index.html DEL index.html
 ECHO ^<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"^> >> index.html
 ECHO ^<html^> >> index.html
 ECHO ^<head^> >> index.html
-ECHO ^<title^>%TITLE%^</title^> >> index.html
+ECHO ^<title^>%INDEX_TITLE%^</title^> >> index.html
 ECHO ^<link rel="stylesheet" href="logtalk.css" type="text/css"^> >> index.html
 ECHO ^</head^> >> index.html
 ECHO ^<body^> >> index.html
-ECHO ^<h1^>%TITLE%^</h1^> >> index.html
+ECHO ^<h1^>%INDEX_TITLE%^</h1^> >> index.html
 ECHO ^<ul^> >> index.html
 
-FOR %%f IN (*.xml) DO ECHO ^<li^>^<a href=^"%%~nf.html^"^>%%~nf^</a^>^</li^> >> index.html
+FOR /f "tokens=1-2 delims=." %%f IN ('DIR /b *.xml') DO ECHO ^<li^>^<a href="%%f.html"^>%%f^</a^>^</li^> >> index.html
 
 ECHO ^</ul^> >> index.html
 
-SET DATETIME=
+ECHO ^<p^>Generated on >> index.html
+DATE/T >> index.html
+TIME/T >> index.html
+ECHO ^</p^> >> index.html
 
-ECHO ^<p^>Generated on %DATETIME%^</p^> >> index.html
 ECHO ^</body^> >> index.html
 ECHO ^</html^> >> index.html
 
