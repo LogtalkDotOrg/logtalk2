228512a8691b852e92aef8953e03812aad145011
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2003-01-28 22:38:10 +0000

    Updated release number to 2.14.8.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@362 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -2,7 +2,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %  Logtalk - Object oriented extension to Prolog
-%  Release 2.14.7
+%  Release 2.14.8
 %
 %  Copyright (c) 1998-2003 Paulo Moura.  All Rights Reserved.
 %
@@ -1088,7 +1088,7 @@ current_logtalk_flag(Flag, Value) :-
 	\+ '$lgt_flag_'(Flag, _),
 	'$lgt_default_flag'(Flag, Value).
 
-current_logtalk_flag(version, version(2, 14, 7)).
+current_logtalk_flag(version, version(2, 14, 8)).
 
 
 
