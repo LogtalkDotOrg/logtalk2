f352d0b86228da64673403ed4b8beadb0f8b319b
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2003-07-01 14:10:09 +0000

    SWI-Prolog hook code for load_files/2.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@475 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ configs/swihook.pl
@@ -0,0 +1,25 @@
+:- system_module.
+
+:- dynamic(prolog_load_file/2).
+:- multifile(prolog_load_file/2).
+
+user:prolog_load_file(_:Spec, Options) :-
+	absolute_file_name(Spec, [extensions([lgt]), access(read), file_errors(fail)], Path),
+	file_directory_name(Path, Dir),
+	file_base_name(Path, File),
+	file_name_extension(Entity, _, File),
+	working_directory(Old, Dir),
+	'$lgt_filter_compiler_options'(Options, Options2),
+	call_cleanup(logtalk_load(Entity, Options2), working_directory(_, Old)).
+
+
+'$lgt_filter_compiler_options'([], []).
+
+'$lgt_filter_compiler_options'([Option| Options], [Option| Options2]) :-
+	functor(Option, Functor, 1),
+	'$lgt_valid_flag'(Functor),
+	!,
+	'$lgt_filter_compiler_options'(Options, Options2).
+
+'$lgt_filter_compiler_options'([_| Options], Options2) :-
+	'$lgt_filter_compiler_options'(Options, Options2).
