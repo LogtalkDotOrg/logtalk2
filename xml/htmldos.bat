05640176022b58dc7b6d087dfdd40088f2ba1681
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-03-08 01:40:30 +0000

    Updated and simplified scripts for new version of XT.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@746 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- xml/htmldos.bat
+++ xml/htmldos.bat
@@ -9,7 +9,7 @@ REM SET XSLT="lgtxhtml.xsl"
 
 ECHO This script converts all .xml files in the current directory to .html
 ECHO files applying the XSLT transformation defined in the $XSLT file
-ECHO using the James Clark XT XSLT Java processor version 20020426a or later.
+ECHO using the James Clark XT XSLT Java processor 20020426a or later version.
 
 FOR %%f IN (*.xml) DO %JAVA_HOME%\bin\java -cp "%XT_PATH%\xt.jar;%XT_PATH%\lib\xp.jar" -Dcom.jclark.xsl.sax.parser=com.jclark.xml.sax.CommentDriver com.jclark.xsl.sax.Driver %%f %XSLT% %%f.html
 
