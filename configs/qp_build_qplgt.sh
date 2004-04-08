6b4e233d85dc34f36c755b6ae114ac13cb7ebc7c
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-04-08 14:14:16 +0000

    Removed config file for older Qu-Prolog 4.3 version. Renamed config file for Qu-Prolog 6.4 to "qu.config".
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@811 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- configs/qp_build_qplgt.sh
+++ configs/qp_build_qplgt.sh
@@ -6,9 +6,9 @@ cd ..
 LOGTALKHOME=`pwd`
 find . -name "*.lgt" -exec perl -pi -e "s/version is (\d)\.(\d)/version is '\1\.\2'/" {} \;
 
-cd $LOGTALKHOME/configs; cp qu64.config qu64.ql
+cd $LOGTALKHOME/configs; cp qu.config qu.ql
 
-echo "fcompile('qu64.ql', [assemble_only(true)]), load(qu64). \
+echo "fcompile('qu.ql', [assemble_only(true)]), load(qu). \
 chdir('../compiler/'), fcompile('logtalk.pl', [assemble_only(true), string_table(256)]), load(logtalk)." | qp -s 2048 -d 1024 -h 2000
 
 
@@ -21,4 +21,4 @@ then
 fi
 
 cd bin
-qc -s 2048 -d 1024 -h 2000 -o qplgt ../configs/qphook.qo ../configs/qu64.qo ../compiler/logtalk.qo
+qc -s 2048 -d 1024 -h 2000 -o qplgt ../configs/qphook.qo ../configs/qu.qo ../compiler/logtalk.qo
