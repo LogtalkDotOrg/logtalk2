09a6bfbca0036a6fb46f95183782c64facff11be
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-04-22 20:36:10 +0000

    Updated to use the env variable LOGTALKHOME.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@886 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/cplgtdirs.sh
+++ scripts/cplgtdirs.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-LGT_LIB_DIR="/usr/local/lib/logtalk"
-LGT_SHARE_LIB="/usr/local/share/logtalk"
-
 echo
-echo This script copies the Logtalk library, xml, and  
-echo examples directories to the user home directory
+echo "This script copies the Logtalk library, xml, and"
+echo "examples directories to the user home directory."
 echo
 
-mkdir -p $HOME/logtalk/examples
-mkdir -p $HOME/logtalk/library
-mkdir -p $HOME/logtalk/xml
-
-cp -RL $LGT_SHARE_LIB/examples $HOME/logtalk/
-cp -RL $LGT_LIB_DIR/library $HOME/logtalk/
-cp -RL $LGT_LIB_DIR/xml $HOME/logtalk/
-
-echo Logtalk library and examples directories copy done
-echo
+if ! [ $LOGTALKHOME ]
+then
+	echo "The env variable LOGTALKHOME must be defined first!"
+else
+	mkdir -p $HOME/logtalk/examples
+	mkdir -p $HOME/logtalk/library
+	mkdir -p $HOME/logtalk/xml
+	cp -RL $LOGTALKHOME/examples $HOME/logtalk/
+	cp -RL $LOGTALKHOME/library $HOME/logtalk/
+	cp -RL $LOGTALKHOME/xml $HOME/logtalk/
+	echo "Finished copying Logtalk directories."
+	echo
+fi
