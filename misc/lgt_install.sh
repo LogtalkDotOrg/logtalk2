d3e2f0f0a46922b8a969e58804982d8b14bc8071
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-04-25 15:54:13 +0000

    Users should use the symbolic link ro the Logtalk installation directory.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@909 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- misc/lgt_install.sh
+++ misc/lgt_install.sh
@@ -17,10 +17,8 @@ mkdir $prefix/lgt2170
 cd ..
 cp -R * $prefix/lgt2170
 
-cd $prefix/lgt2170
-chmod -R go-w,a+r .
-
-cd ..
+cd $prefix
+chmod -R go-w,a+r lgt2170
 chmod a+x lgt2170
 ln -sf lgt2170 logtalk
 
@@ -28,7 +26,8 @@ cd bin
 ln -sf ../lgt2170/misc/cplgtdirs.sh cplgtdirs.sh
 
 echo "Installation completed."
-echo "Users should define the env variable LOGTALKHOME pointing to $prefix/lgt2170"
-echo "and then run the shell script cplgtdirs.sh in order to make a local copy of "
-echo "Logtalk examples, xml, and library directories in ~/logtalk."
+echo "Users should define the environment variable LOGTALKHOME pointing"
+echo "to $prefix/logtalk and then run the shell script cplgtdirs.sh in"
+echo "order to make a local copy of the Logtalk examples, library, and"
+echo "xml directories in ~/logtalk."
 echo
