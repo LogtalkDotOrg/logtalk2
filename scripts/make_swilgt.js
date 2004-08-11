bc33e2be8e82b32da34a9988670a69d5d2a9d805
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-08-11 14:51:57 +0000

    More bug fixes.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1330 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/make_swilgt.js
+++ scripts/make_swilgt.js
@@ -41,7 +41,6 @@ var f = fso.CreateTextFile(logtalk_home + "\\bin\\logtalkswi.pl", true);
 
 f.WriteLine(":- set_prolog_flag(character_escapes, false).");
 f.WriteLine(":- include('" + logtalk_home + "\\configs\\swihook.pl').");
-f.WriteLine(":- system_module.");
 f.WriteLine(":- include('" + logtalk_home + "\\configs\\swi.config').");
 f.WriteLine(":- include('" + logtalk_home + "\\compiler\\logtalk.pl').");
 f.WriteLine(":- set_prolog_flag(character_escapes, true).");
