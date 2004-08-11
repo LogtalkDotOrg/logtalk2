ea970713848d3f7a51c3a38d20c45151a8a7fc73
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-08-11 15:08:53 +0000

    More bug fixes.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1334 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- misc/make_swilgt.js
+++ misc/make_swilgt.js
@@ -38,18 +38,23 @@ if (!fso.FolderExists(logtalk_home + "\\bin"))
 	fso.CreateFolder(logtalk_home + "\\bin");
 
 var f = fso.CreateTextFile(logtalk_home + "\\bin\\logtalkswi.pl", true);
+f.WriteLine(":- system_module.");
+f.Close();
+
+WshShell.Run("type " + logtalk_home + "\\compiler\\logtalk.pl" + " >> " + logtalk_home + "\\bin\\logtalkswi.pl", true);
+
+f = fso.CreateTextFile(logtalk_home + "\\bin\\logtalkswi.rc", true);
 
 f.WriteLine(":- set_prolog_flag(character_escapes, false).");
 f.WriteLine(":- consult('" + logtalk_home + "\\configs\\swihook.pl').");
 f.WriteLine(":- consult('" + logtalk_home + "\\configs\\swi.config').");
-// f.WriteLine(":- system_module.");
-f.WriteLine(":- consult('" + logtalk_home + "\\compiler\\logtalk.pl').");
+f.WriteLine(":- consult('" + logtalk_home + "\\bin\\logtalkswi.pl').");
 f.WriteLine(":- set_prolog_flag(character_escapes, true).");
 f.Close();
 
 var ProgramsPath = WshShell.SpecialFolders("AllUsersPrograms");
 var link = WshShell.CreateShortcut(ProgramsPath + "\\Swilgt.lnk");
-link.Arguments = "-f "+ logtalk_home + "\\bin\\logtalkswi.pl";
+link.Arguments = "-f "+ logtalk_home + "\\bin\\logtalkswi.rc";
 link.Description = "Logtalk & SWI-Prolog";
 link.IconLocation = "app.exe,1";
 link.TargetPath = WshShell.RegRead("HKEY_LOCAL_MACHINE\\Software\\SWI\\Prolog\\home") + "\\bin\\plwin.exe";
