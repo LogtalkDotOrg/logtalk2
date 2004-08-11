6fc166ccc18f30765de8e8df6f8ea4f0c2b5a734
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-08-11 17:56:53 +0000

    More bug fixes.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1369 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/make_eclipselgt.js
+++ scripts/make_eclipselgt.js
@@ -43,13 +43,13 @@ var f = fso.CreateTextFile(logtalk_home + "\\bin\\lgtceclipse.pl", true);
 
 f.WriteLine(":- pragma(system).");
 f.WriteLine(":- pragma(nodebug).");
-f.WriteLine(":- ensure_loaded(library(toplevel)).");
 f.Close();
 
 WshShell.Run("cmd /c type " + logtalk_home + "\\compiler\\logtalk.pl" + " >> " + logtalk_home + "\\bin\\lgtceclipse.pl", true);
 
 f = fso.CreateTextFile(logtalk_home + "\\bin\\logtalkeclipse.pl", true);
 
+f.WriteLine(":- ensure_loaded(library(toplevel)).");
 f.WriteLine(":- consult('" + logtalk_home + "\\\\configs\\\\eclipseiso.config').");
 f.WriteLine(":- consult('" + logtalk_home + "\\\\bin\\\\lgtceclipse.pl').");
 f.Close();
