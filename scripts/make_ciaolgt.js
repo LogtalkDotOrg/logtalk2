dab226ecc6409b32602c83d98ffd5307eef036ea
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-08-17 19:18:03 +0000

    Added Windows JScript script for easy integration of Logtalk and CIAO Prolog.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1411 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ scripts/make_ciaolgt.js
@@ -0,0 +1,92 @@
+// =================================================================
+// Logtalk - Object oriented extension to Prolog
+// Release 2.20.1
+//
+// Copyright (c) 1998-2004 Paulo Moura.  All Rights Reserved.
+// =================================================================
+
+if (WScript.Arguments.Unnamed.Length > 0) {
+	usage_help();
+	WScript.Quit(0);
+}
+
+WScript.Echo('');
+WScript.Echo('Creating a shortcut named "Logtalk - CIAO" for running Logtalk');
+WScript.Echo('with CIAO...');
+WScript.Echo('');
+
+var WshShell = new ActiveXObject("WScript.Shell");
+
+var prolog_path = WshShell.RegRead("HKLM\\Software\\Ciao Prolog\\ciao_dir") + "\\shell\\ciaosh.exe";
+
+var FSObject = new ActiveXObject("Scripting.FileSystemObject");
+
+if (!FSObject.FileExists(prolog_path)) {
+	WScript.Echo("Error! Cannot find ciaosh.exe at the expected place!");
+	WScript.Quit(1);
+}
+
+var WshProcessEnv = WshShell.Environment("PROCESS");
+var WshSystemEnv = WshShell.Environment("SYSTEM");
+var WshUserEnv = WshShell.Environment("USER");
+var logtalk_home;
+
+if (WshProcessEnv.Item("LOGTALKHOME"))
+	logtalk_home = WshProcessEnv.Item("LOGTALKHOME");
+else if (WshSystemEnv.Item("LOGTALKHOME"))
+	logtalk_home = WshSystemEnv.Item("LOGTALKHOME");
+else if (WshUserEnv.Item("LOGTALKHOME"))
+	logtalk_home = WshUserEnv.Item("LOGTALKHOME")
+else {
+	WScript.Echo("Error! The system environment variable LOGTALKHOME must be defined first!");
+	usage_help();
+	WScript.Quit(1);
+}
+
+logtalk_home = logtalk_home.replace(/\\/g, "\\\\");
+
+if (!FSObject.FolderExists(logtalk_home + "\\bin")) 
+	FSObject.CreateFolder(logtalk_home + "\\bin");
+
+var f = FSObject.CreateTextFile(logtalk_home + "\\bin\\logtalkciao.pl", true);
+
+f.WriteLine(":- ensure_loaded('\$LOGTALKHOME/configs/ciao_aux.config').");
+f.WriteLine(":- ensure_loaded('\$LOGTALKHOME/compiler/logtalk.pl').");
+f.WriteLine(":- op(600, xfy, ::).");
+f.WriteLine(":- op(600,  fy, ::).");
+f.WriteLine(":- op(600,  fy, ^^).");
+f.WriteLine(":- op(200,  fy, +).");
+f.WriteLine(":- op(200,  fy, ?).");
+f.WriteLine(":- op(200,  fy, @).");
+f.WriteLine(":- op(200,  fy, -).");
+f.Close();
+
+var ProgramsPath = WshShell.SpecialFolders("AllUsersPrograms");
+var link = WshShell.CreateShortcut(ProgramsPath + "\\Logtalk - CIAO.lnk");
+link.Arguments = "-l %LOGTALKHOME%\\bin\\logtalkciao.pl";
+link.Description = "Runs Logtalk with CIAO";
+link.IconLocation = "app.exe,1";
+link.TargetPath = prolog_path;
+link.WindowStyle = 1;
+link.WorkingDirectory = logtalk_home;
+link.Save();
+
+WScript.Echo('Done. The "Logtalk - CIAO" shortcut was been added to the');
+WScript.Echo('Start Menu Programs. Make sure that the LOGTALKHOME environment');
+WScript.Echo('variable is defined for all users wishing to use the shortcut.');
+WScript.Echo('');
+
+WScript.Quit(0);
+
+function usage_help() {
+	WScript.Echo('');
+	WScript.Echo('This script creates a shortcut named "Logtalk - CIAO" for running Logtalk');
+	WScript.Echo('with CIAO. The script must be run by a user with administrative rights.');
+	WScript.Echo('The LOGTALKHOME environment variable must be defined before running this');
+	WScript.Echo('script.');
+	WScript.Echo('');
+	WScript.Echo('Usage:');
+	WScript.Echo('  ' + WScript.ScriptName + ' help');
+	WScript.Echo('  ' + WScript.ScriptName);
+	WScript.Echo('');
+}
