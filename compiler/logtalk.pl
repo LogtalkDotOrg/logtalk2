1ffb402c44c5b69a7ba2b70b4b59e59de012add8
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-10-25 21:10:15 +0000

    Updated release number to 2.21.4.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1643 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -1123,7 +1123,7 @@ current_logtalk_flag(Flag, Value) :-
 	'$lgt_default_flag'(Flag, Value),
 	\+ '$lgt_current_flag_'(Flag, _).
 
-current_logtalk_flag(version, version(2, 21, 3)).
+current_logtalk_flag(version, version(2, 21, 4)).
 
 
 
