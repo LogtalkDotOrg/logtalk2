0c82d283c1c36c16a5916e227e71488545d564f3
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2004-12-01 18:08:47 +0000

    Updated release number to 2.22.1.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@1763 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- configs/swihook.pl
+++ configs/swihook.pl
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %  Logtalk - Object oriented extension to Prolog
-%  Release 2.22.0
+%  Release 2.22.1
 %
 %  integration code for SWI Prolog 3.3.x and later versions
 %  to compile and load Logtalk files using SWI Prolog consult/1
