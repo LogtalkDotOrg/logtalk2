230530edc01fba0202f5e258b286381a208093df
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2005-05-22 13:59:55 +0000

    Individual scripts return zero when they succedded!
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2143 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/makeall_lgt.js
+++ scripts/makeall_lgt.js
@@ -23,45 +23,49 @@ else {
 	WScript.Quit(1);
 }
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_ciaolgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_ciaolgt.js", true) == 0)
 	WScript.Echo('Logtalk - CIAO shortcut created');
 else
 	WScript.Echo('Logtalk - CIAO shortcut creation failed (CIAO Prolog not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_eclipselgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_eclipselgt.js", true) == 0)
 	WScript.Echo('Logtalk - ECLiPSe shortcut created');
 else
 	WScript.Echo('Logtalk - ECLiPSe shortcut creation failed (ECLiPSe not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_gplgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_gplgt.js", true) == 0)
 	WScript.Echo('Logtalk - GNU Prolog shortcut created');
 else
 	WScript.Echo('Logtalk - GNU Prolog shortcut creation failed (GNU Prolog not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_plclgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_plclgt.js", true) == 0)
 	WScript.Echo('Logtalk - K-Prolog shortcut created');
 else
 	WScript.Echo('Logtalk - K-Prolog shortcut creation failed (K-Prolog not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_sicstuslgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_sicstuslgt.js", true) == 0)
 	WScript.Echo('Logtalk - SICStus shortcut created');
 else
 	WScript.Echo('Logtalk - SICStus shortcut creation failed (SICStus Prolog not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_swilgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_swilgt.js", true) == 0)
 	WScript.Echo('Logtalk - SWI-Prolog shortcut created');
 else
 	WScript.Echo('Logtalk - SWI-Prolog shortcut creation failed (SWI-Prolog not installed?)');
 
-if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_yaplgt.js", true))
+if (WshShell.Run("cscript %LOGTALKHOME%\\scripts\\make_yaplgt.js", true) == 0)
 	WScript.Echo('Logtalk - YAP shortcut created');
 else
 	WScript.Echo('Logtalk - YAP shortcut creation failed (YAP not installed?)');
 
-WScript.Echo('Done. Done. Links to the created scripts was been added to the');
-WScript.Echo('Start Menu Programs. Make sure that the environment variables');
-WScript.Echo('LOGTALKHOME and LOGTALKUSER are defined for all users wishing');
-WScript.Echo('to use the shortcuts.');
+WScript.Echo('');
+WScript.Echo('Done. Links to the created scripts was been added to the Start Menu');
+WScript.Echo('Programs.  Make sure that the environment variables LOGTALKHOME and');
+WScript.Echo('LOGTALKUSER are defined for all users wishing to use the shortcuts.');
+WScript.Echo('');
+WScript.Echo('If you get an unexpected failure to create a shortcut for one of the');
+WScript.Echo('above Prolog compilers, please consult the NOTES file on the scripts'),
+WScript.Echo('directory or try to run the corresponding script individually.');
 WScript.Echo('');
 
 WScript.Quit(0);
