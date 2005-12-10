d4097752b7c9ce41e832af23cce6a6a65eb55721
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2005-12-10 11:33:52 +0000

    Added a config file for the current XSB CVS version and the corresponding integration shell scripts (for both Windows and POSIX systems).
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2526 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ scripts/make_xsbcvslgt.js
@@ -0,0 +1,101 @@
+// =================================================================
+// Logtalk - Object oriented extension to Prolog
+// Release 2.26.2
+//
+// Copyright (c) 1998-2005 Paulo Moura.  All Rights Reserved.
+// =================================================================
+
+var prolog_path = "C:\\Program Files\\XSB\\bin\\xsb.exe";
+
+if (WScript.Arguments.Unnamed.Length > 0) {
+	usage_help();
+	WScript.Quit(0);
+}
+
+WScript.Echo('');
+WScript.Echo('Creating a shortcut named "Logtalk - XSB" for running Logtalk');
+WScript.Echo('with XSB...');
+WScript.Echo('');
+
+var FSObject = new ActiveXObject("Scripting.FileSystemObject");
+
+if (!FSObject.FileExists(prolog_path)) {
+	WScript.Echo("Error! Cannot find xsb.exe at the expected place!");
+	WScript.Quit(1);
+}
+
+var WshShell = new ActiveXObject("WScript.Shell");
+
+var WshSystemEnv = WshShell.Environment("SYSTEM");
+var WshUserEnv = WshShell.Environment("USER");
+var logtalk_home;
+
+if (WshSystemEnv.Item("LOGTALKHOME"))
+	logtalk_home = WshSystemEnv.Item("LOGTALKHOME");
+else if (WshUserEnv.Item("LOGTALKHOME"))
+	logtalk_home = WshUserEnv.Item("LOGTALKHOME")
+else {
+	WScript.Echo("Error! The environment variable LOGTALKHOME must be defined first!");
+	usage_help();
+	WScript.Quit(1);
+}
+
+if (!FSObject.FolderExists(logtalk_home)) {
+	WScript.Echo("The environment variable LOGTALKHOME points to a non-existing directory!");
+	WScript.Echo("Its current value is: %LOGTALKHOME%");
+	WScript.Echo("The variable must be set to your Logtalk installation directory!");
+	WScript.Echo("");
+	usage_help();
+	WScript.Quit(1);
+}
+
+logtalk_home = logtalk_home.replace(/\\/g, "\\\\");
+
+if (!FSObject.FolderExists(logtalk_home + "\\bin")) 
+	FSObject.CreateFolder(logtalk_home + "\\bin");
+
+FSObject.CopyFile(logtalk_home + "\\compiler\\logtak.pl", logtalk_home + "\\bin\\logtak.P").
+
+var f = FSObject.CreateTextFile(logtalk_home + "\\bin\\logtalk_xsb.P", true);
+
+f.WriteLine(":- reconsult('$LOGTALKUSER\\\\configs\\\\xsb.P').");
+f.WriteLine(":- reconsult('$LOGTALKHOME\\\\bin\\\\logtalk.P').");
+f.WriteLine(":- reconsult('$LOGTALKUSER\\\\libpaths\\\\libpaths.P').");
+f.Close();
+
+var ProgramsPath = WshShell.SpecialFolders("AllUsersPrograms");
+
+if (!FSObject.FolderExists(ProgramsPath + "\\Logtalk")) 
+	FSObject.CreateFolder(ProgramsPath + "\\Logtalk");
+
+var link = WshShell.CreateShortcut(ProgramsPath + "\\Logtalk\\Logtalk - XSB.lnk");
+link.Arguments = "-l %LOGTALKHOME%\\bin\\logtalk_xsb.P";
+link.Description = "Runs Logtalk with XSB";
+link.IconLocation = "app.exe,1";
+link.TargetPath = prolog_path;
+link.WindowStyle = 1;
+link.WorkingDirectory = logtalk_home;
+link.Save();
+
+WScript.Echo('Done. The "Logtalk - XSB" shortcut was been added to the Start Menu');
+WScript.Echo('Programs.  Make sure that the environment variables LOGTALKHOME and');
+WScript.Echo('LOGTALKUSER are defined for all users wishing to use the shortcut.');
+WScript.Echo('');
+WScript.Echo('Users must run the batch script "cplgtdirs" before using the');
+WScript.Echo('"Logtalk - XSB" shortcut.');
+WScript.Echo('');
+
+WScript.Quit(0);
+
+function usage_help() {
+	WScript.Echo('');
+	WScript.Echo('This script creates a shortcut named "Logtalk - XSB" for running Logtalk');
+	WScript.Echo('with XSB. The script must be run by a user with administrative rights.');
+	WScript.Echo('The LOGTALKHOME environment variable must be defined before running this');
+	WScript.Echo('script.');
+	WScript.Echo('');
+	WScript.Echo('Usage:');
+	WScript.Echo('  ' + WScript.ScriptName + ' help');
+	WScript.Echo('  ' + WScript.ScriptName);
+	WScript.Echo('');
+}
