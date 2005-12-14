5a8177834e50251308578fdc2169fd74ce1e92ad
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2005-12-14 14:06:29 +0000

    Added option for increasing the tag trail size while compiling the Logtalk files.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2540 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/make_qplgt.sh
+++ scripts/make_qplgt.sh
@@ -44,10 +44,10 @@ cd configs
 cp qu.config qu.ql
 echo "fcompile('qu.ql', [assemble_only(true)]), load(qu). \
 chdir('../compiler/'), fcompile('logtalk.pl', [assemble_only(true), string_table(256)]), load(logtalk). \
-chdir('../libpaths/'), fcompile('libpaths.pl', [assemble_only(true)]), load(libpaths)." | qp -s 2048 -d 1024 -h 2000
+chdir('../libpaths/'), fcompile('libpaths.pl', [assemble_only(true)]), load(libpaths)." | qp -T 16 -s 2048 -d 1024 -h 2048
 qc -c qphook.ql
 cd ../bin
-qc -s 2048 -d 1024 -h 2000 -o qplgt ../configs/qphook.qo ../configs/qu.qo ../compiler/logtalk.qo  ../libpaths/libpaths.qo
+qc -T 16 -s 2048 -d 1024 -h 2048 -o qplgt ../configs/qphook.qo ../configs/qu.qo ../compiler/logtalk.qo  ../libpaths/libpaths.qo
 chmod a+x qplgt
 ln -sf $LOGTALKHOME/bin/qplgt $prefix/bin/qplgt
 rm ../configs/qu.ql
