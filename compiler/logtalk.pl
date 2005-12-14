3de89cdf7da87dc6ebb06000df035799bd6d52d7
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2005-12-14 14:42:51 +0000

    Removed the read-only compiler flag "iso_initialization_dir".
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2542 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -9970,8 +9970,8 @@ current_logtalk_flag(version, version(2, 26, 2)).
 	'$lgt_default_flag'(events, Events), write(', events: '), write(Events), nl,
 	write('Read-only compilation flags:'), nl,
 	'$lgt_default_flag'(supports_break_predicate, Break), write('  supports_break_predicate: '), write(Break),
-	'$lgt_default_flag'(startup_message, Startup), write('  startup_message: '), write(Startup),
-	'$lgt_default_flag'(altdirs, Altdirs), write(', altdirs: '), write(Altdirs),
+	'$lgt_default_flag'(startup_message, Startup), write(', startup_message: '), write(Startup), nl,
+	'$lgt_default_flag'(altdirs, Altdirs), write('  altdirs: '), write(Altdirs),
 	'$lgt_default_flag'(supports_encoding_dir, Encodings), write(', supports_encoding_dir: '), write(Encodings), nl, nl.
 
 '$lgt_default_flags'(verbose) :-
