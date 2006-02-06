09b72c6e34f7dc67f9a00c74b32dbacdcfb96273
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-02-06 16:51:34 +0000

    Hide some more runtime dynamic predicates.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2652 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/make_yaplgt.sh
+++ scripts/make_yaplgt.sh
@@ -46,7 +46,7 @@ echo ":- reconsult('\$LOGTALKHOME/compiler/logtalk.pl')." >> logtalk_yap.rc
 echo ":- reconsult('\$LOGTALKUSER/libpaths/libpaths.pl')." >> logtalk_yap.rc
 
 echo "#/bin/sh" > yaplgt
-echo "yap -l \$LOGTALKHOME/bin/logtalk_yap.rc" >> yaplgt
+echo "yap -s 49152 -h 16384 -t 1024 -l \$LOGTALKHOME/bin/logtalk_yap.rc" >> yaplgt
 chmod a+x yaplgt
 ln -sf $LOGTALKHOME/bin/yaplgt $prefix/bin/yaplgt
 echo "Done. A link to the script was been created in $prefix/bin."
