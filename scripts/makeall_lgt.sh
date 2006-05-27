c30c0df217b332b0833519e32c08102ad615af3a
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-05-27 10:43:36 +0000

    Simplified syntax for broad compatibility.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2788 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/makeall_lgt.sh
+++ scripts/makeall_lgt.sh
@@ -39,70 +39,70 @@ if ! [ -d "$prefix" ]; then
 fi
 
 ./make_ciaolgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "ciaolgt script created"
 else
 	echo "ciaolgt script creation failed"
 fi
 
 ./make_eclipselgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "eclipselgt script created"
 else
 	echo "eclipselgt script creation failed"
 fi
 
 ./make_gplgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "gplgt script created"
 else
 	echo "gplgt script creation failed"
 fi
 
 ./make_plclgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "plclgt script created"
 else
 	echo "plclgt script creation failed"
 fi
 
 ./make_qplgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "qplgt script created"
 else
 	echo "qplgt script creation failed"
 fi
 
 ./make_sicstuslgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "sicstuslgt script created"
 else
 	echo "sicstuslgt script creation failed"
 fi
 
 ./make_swilgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "swilgt script created"
 else
 	echo "swilgt script creation failed"
 fi
 
 ./make_xsblgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "xsblgt script created"
 else
 	echo "xsblgt script creation failed"
 fi
 
 ./make_xsbcvslgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "xsbcvslgt script created"
 else
 	echo "xsbcvslgt script creation failed"
 fi
 
 ./make_yaplgt.sh $prefix > /dev/null 2> /dev/null
-if [[ $? -eq 0 ]]; then
+if [ $? = 0 ] ; then
 	echo "yaplgt script created"
 else
 	echo "yaplgt script creation failed"
