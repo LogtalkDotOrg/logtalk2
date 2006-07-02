d063adcbf567af2de6d5bfe933414bc0627e64e2
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-07-02 17:16:29 +0000

    Corrected typo.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@2882 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- xml/lgt2html.js
+++ xml/lgt2html.js
@@ -236,7 +236,7 @@ function create_index_file() {
 	var month = today.getMonth() + 1;
 	if (month < 10)
         month = "0" + month;
-	day   = today.getDate();
+	var day = today.getDate();
 	if (day < 10)
         day = "0" + day;
 	strToday = year + "/" + month + "/" + day;
