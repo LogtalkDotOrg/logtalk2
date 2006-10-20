91141c923c6370d0408d19721e865ecc03899d23
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-10-20 11:18:48 +0000

    Added a XPCE hook file ("configs/xpcehook.pl") supporting Logtalk message sending goals as XPCE call-back goals.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3062 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ configs/xpcehook.pl
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Logtalk - Object oriented extension to Prolog
+%  Release 2.28.2
+%
+%  integration code for XPCE 6.6.21 and later versions supporting Logtalk 
+%  message sending goals as call-backs goals by using the syntax:
+% 
+%    logtalk(Object, MessageFunctor, MessageArg1, MessageArg2, ...)
+%
+%  as an alternative to XPCE's message(...) call-backs
+%
+%  last updated: October 20, 2006
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+:- use_module(library(pce)).
+
+
+:- pce_begin_class(logtalk, message).
+
+	initialise(Msg, Obj:prolog, Functor:prolog, Args:unchecked ...) :->
+		Pred =.. [Functor| Args],
+		SuperMsg =.. [initialise, @prolog, call, prolog(Obj::Pred)],
+		send_super(Msg, SuperMsg).
+
+:- pce_end_class.
