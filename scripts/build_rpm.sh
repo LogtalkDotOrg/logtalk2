cc8f5cf54d0607d9c4481882a38de6d67507acf9
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-12-25 10:50:18 +0000

    Added a simple shell script for building Linux RPMs.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3238 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ scripts/build_rpm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## =================================================================
+## Logtalk - Object oriented extension to Prolog
+## Release 2.29.1
+##
+## Copyright (c) 1998-2006 Paulo Moura.  All Rights Reserved.
+## =================================================================
+
+dir="$PWD"
+
+lgt_install.sh
+
+cd /usr/local/logtalk
+scripts/cleandist.sh
+
+cd ..
+tar -czf lgt2291.tgz lgt2291
+mv lgt2291.tgz /usr/src/redhat/SOURCES
+
+cd "$dir"
+rpmbuild -ba --target=noarch-*-linux logtalk.spec
+
+cd /usr/src/redhat/RPMS/noarch
