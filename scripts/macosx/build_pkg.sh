512a552786ce073ab0556bf5fe5d0572fdf6f40d
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2006-12-26 00:44:56 +0000

    Files used when building MacOS X installer packages.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3248 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/macosx/build_pkg.sh
+++ scripts/macosx/build_pkg.sh
@@ -11,6 +11,7 @@ cd ..
 dir="$PWD"
 
 port uninstall logtalk
+port clean --all logtalk
 
 LOGTALKHOME=/opt/local/logtalk ./lgt_uninstall.sh
 ./lgt_install.sh /opt/local
@@ -25,6 +26,7 @@ chmod a-x xml/*.js
 cd ..
 tar -czf lgt2291.tgz lgt2291
 md5="`md5 -q lgt2291.tgz`"
+mkdir -p /opt/local/var/db/dports/distfiles/logtalk
 mv lgt2291.tgz /opt/local/var/db/dports/distfiles/logtalk/lgt2291.tgz
 
 cd "$dir"
@@ -32,20 +34,15 @@ LOGTALKHOME=/opt/local/logtalk ./lgt_uninstall.sh
 
 cd /opt/local/var/db/dports/sources/rsync.rsync.darwinports.org_dpupdate_dports/lang/logtalk/
 cp Portfile Portfile.old
-sed -e 's/\(^version[[:space:]]*\).*/\1 2.29.1/' -i '' Portfile
-sed -e "s/\(^checksums[[:space:]]*md5[[:space:]]*\)\([[:alnum:]]\{32\}\)/\1 $md5/" -i '' Portfile
-sed -e 's/\(^distname[[:space:]]*\).*/\1 lgt2291/' -i '' Portfile
+sed -e 's/^version.*/version 2.29.1/' -i '' Portfile
+sed -e "s/^checksums.*/checksums md5 $md5/" -i '' Portfile
+sed -e 's/^distname.*/distname lgt2291/' -i '' Portfile
 
 port install logtalk
 port pkg logtalk
 
-cd /opt/local/var/db/dports/build/_opt_local_var_db_dports_sources_rsync.rsync.darwinports.org_dpupdate_dports_lang_logtalk/work/logtalk-2.29.1.pkg/Contents/Resources
-
-cp "$dir"/macosx/License.html .
-cp "$dir"/macosx/ReadMe.html .
-cp "$dir"/macosx/Welcome.html .
-
-cd ../../..
+cd /opt/local/var/db/dports/build/_opt_local_var_db_dports_sources_rsync.rsync.darwinports.org_dpupdate_dports_lang_logtalk/work
+rm -fr $HOME/Desktop/logtalk-2.29.1.pkg
 mv logtalk-2.29.1.pkg $HOME/Desktop
 
 port uninstall logtalk
