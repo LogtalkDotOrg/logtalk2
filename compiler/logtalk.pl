0ffdc52a188b4967851447d71fe62a5185a63c6f
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2007-01-08 11:06:40 +0000

    Fix typo.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3292 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -11581,7 +11581,7 @@ current_logtalk_flag(version, version(2, 29, 2)).
 	\+ \+ (
 		thread_peek_message(Thread, '$lgt_competing_id'(Goal, Sender, This, Self, Id)),
 		thread_get_message(Thread, '$lgt_competing_id'(Goal, Sender, This, Self, Id)),
-		catch((thread_signal(Id, thread_exit(true)), thread_join(Id, _)), _, true),
+		catch((thread_signal(Id, thread_exit(true)), thread_join(Id, _)), _, true)
 		),
 	!,
 	'$lgt_mt_kill_competing_threads'(Thread, Goal, Sender, This, Self).
