5e4576edf30340457fa7c0ee6631d4460dc81b23
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2007-05-15 09:12:48 +0000

    Simple example of using tabling directives within objects.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3712 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- libpaths/libpaths.pl
+++ libpaths/libpaths.pl
@@ -67,5 +67,6 @@
 	 assertz(logtalk_library_path(sorting, threads('sorting/'))),
 	 assertz(logtalk_library_path(sync, threads('sync/'))),
 	 assertz(logtalk_library_path(symdiff, examples('symdiff/'))),
+	 assertz(logtalk_library_path(tabling, examples('tabling/'))),
 	 assertz(logtalk_library_path(threads, examples('threads/'))),
 	 assertz(logtalk_library_path(viewpoints, examples('viewpoints/'))))).
