1193f31eaffad386f4db09e0dcb98702557dd505
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2007-06-14 22:29:17 +0000

    Updated release number to 2.30.2.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3758 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -2,7 +2,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %  Logtalk - Open source object-oriented logic programming language
-%  Release 2.30.1
+%  Release 2.30.2
 %
 %  Copyright (c) 1998-2007 Paulo Moura.  All Rights Reserved.
 %
@@ -1581,7 +1581,7 @@ current_logtalk_flag(Flag, Value) :-
 	'$lgt_default_flag'(Flag, Value),
 	\+ '$lgt_current_flag_'(Flag, _).
 
-current_logtalk_flag(version, version(2, 30, 1)).
+current_logtalk_flag(version, version(2, 30, 2)).
 
 
 
