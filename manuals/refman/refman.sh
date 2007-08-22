7aaedf85ad38d6878a4213a96e905fa21329142d
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2007-08-22 14:17:36 +0000

    Updated the PDF documentation scripts to use the latest version of CSSToXSLFO (1.5).
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@3820 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- manuals/refman/refman.sh
+++ manuals/refman/refman.sh
@@ -7,7 +7,7 @@
 ## Copyright (c) 1998-2007 Paulo Moura.  All Rights Reserved.
 ## ================================================================
 
-css2xslfo=/Applications/XML/CSSToXSLFO/css2xslfo1_4_2.jar
+css2xslfo=/Applications/XML/CSSToXSLFO/css2xslfo1_5.jar
 
 xslt_proc=xsltproc
 # xslt_proc=xalan
