32130d520bc9e2cde3b9467950b75c6bfe718e81
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-01-17 19:27:28 +0000

    Added a POSIX integration script for Quintus Prolog.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4021 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ integration/logtalk_quintus.pl
@@ -0,0 +1,14 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Logtalk - Open source object-oriented logic programming language
+%  Release 2.31.2
+%
+%  Copyright (c) 1998-2008 Paulo Moura.  All Rights Reserved.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+:- compile('$LOGTALKUSER/configs/quintus.config').
+:- compile('$LOGTALKHOME/compiler/logtalk.pl').
+:- compile('$LOGTALKUSER/libpaths/libpaths.pl').
