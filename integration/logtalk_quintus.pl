8d158515b450c4c9128f5b18f3ab53ed23228542
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-01-23 10:10:37 +0000

    Found a nasty hack for workaround the lack of support for environment variables in file names in Quintus Prolog.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4044 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/logtalk_quintus.pl
+++ integration/logtalk_quintus.pl
@@ -9,6 +9,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-:- unix(argv([_, LOGTALKUSER| _])), unix(cd(LOGTALKUSER)), compile('configs/quintus.config').
-:- unix(argv([LOGTALKHOME| _])), unix(cd(LOGTALKHOME)), compile('compiler/logtalk.pl').
-:- unix(argv([_, LOGTALKUSER| _])), unix(cd(LOGTALKUSER)), compile('libpaths/libpaths.pl').
+:- 	unix(args([LOGTALKHOME, LOGTALKUSER| _])),	% hack for workaround the lack of support for environment variables in file names
+	atom_chars(LOGTALKHOME, LH),
+	atom_chars(LOGTALKUSER, LU),
+	atom_chars('/configs/quintus.config', LC), append(LU, LC, L1), atom_chars(ConfigFile, L1), compile(ConfigFile),
+	atom_chars('/compiler/logtalk.pl', LL), append(LH, LL, L2), atom_chars(CompilerFile, L2), compile(CompilerFile),
+	atom_chars('/libpaths/libpaths.pl', LP), append(LU, LP, L3), atom_chars(LibpathsFile, L3), compile(LibpathsFile).
