402fa151d584b066e145fa87f6040417a47ffbf5
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-02-09 19:49:34 +0000

    Simplified Prolog integration scripts by moving the safety checks into its own file that gets included in every script.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4072 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/eclipselgt.sh
+++ integration/eclipselgt.sh
@@ -7,63 +7,6 @@
 ## Copyright (c) 1998-2008 Paulo Moura.  All Rights Reserved.
 ## ================================================================
 
-if ! [ "$LOGTALKHOME" ]; then
-	echo "The environment variable LOGTALKHOME should be defined first, pointing"
-	echo "to your Logtalk installation directory!"
-	echo "Trying the default locations for the Logtalk installation..."
-	if [ -d "/usr/local/share/logtalk" ]; then
-		LOGTALKHOME=/usr/local/share/logtalk
-		echo "... using Logtalk installation found at /usr/local/share/logtalk"
-	elif [ -d "/usr/share/logtalk" ]; then
-		LOGTALKHOME=/usr/share/logtalk
-		echo "... using Logtalk installation found at /usr/share/logtalk"
-	elif [ -d "/opt/local/share/logtalk" ]; then
-		LOGTALKHOME=/opt/local/share/logtalk
-		echo "... using Logtalk installation found at /opt/local/share/logtalk"
-	elif [ -d "/opt/share/logtalk" ]; then
-		LOGTALKHOME=/opt/share/logtalk
-		echo "... using Logtalk installation found at /opt/share/logtalk"
-	else
-		echo "... unable to locate Logtalk installation directory!"
-		echo
-		exit 1
-	fi
-	echo
-elif ! [ -d "$LOGTALKHOME" ]; then
-	echo "The environment variable LOGTALKHOME points to a non-existing directory!"
-	echo "Its current value is: $LOGTALKHOME"
-	echo "The variable must be set to your Logtalk installation directory!"
-	echo
-	exit 1
-fi
-export LOGTALKHOME
-
-if ! [ "$LOGTALKUSER" ]; then
-	echo "The environment variable LOGTALKUSER should be defined first, pointing"
-	echo "to your Logtalk user directory!"
-	echo "Trying the default location for the Logtalk user directory..."
-	echo
-	export LOGTALKUSER=$HOME/logtalk
-fi
-
-if [ -d "$LOGTALKUSER" ]; then
-	if ! [ -a "$LOGTALKUSER/VERSION.txt" ]; then
-		echo "Logtalk user directory at $LOGTALKUSER is outdated!"
-		echo "Creating an up-to-date Logtalk user directory..."
-		cplgtdirs
-	else
-		current=`cat $LOGTALKUSER/VERSION.txt | sed 's/\.//g'`
-		if [ $current -lt 2313 ]; then
-			echo "Logtalk user directory at $LOGTALKUSER is outdated!"
-			echo "Creating an up-to-date Logtalk user directory..."
-			cplgtdirs
-		fi
-	fi
-else
-	echo "Cannot find \$LOGTALKUSER directory! Creating a new Logtalk user directory"
-	echo "by running the \"cplgtdirs\" shell script:"
-	cplgtdirs
-fi
-echo
+source $LOGTALKHOME/integration/checks.sh
 
 exec eclipse -b "$LOGTALKHOME/integration/logtalk_eclipse.pl" "$@"
