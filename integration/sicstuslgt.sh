1f97121d8af8c69713dd6a6de1a8d1786173561b
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-02-13 12:39:43 +0000

    Removed the "check.sh" auxiliary script. As silly as it seems, there is no reliable solution to put the checks in their own file that would be source'd within each individual integration script!
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4076 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/sicstuslgt.sh
+++ integration/sicstuslgt.sh
@@ -7,7 +7,68 @@
 ## Copyright (c) 1998-2008 Paulo Moura.  All Rights Reserved.
 ## ================================================================
 
-source $LOGTALKHOME/integration/checks.sh
+## As silly as it seems, there is no reliable solution to put the following
+## checks in their own file that would be source'd within each individual  
+## integration script!!!
+
+if ! [ "$LOGTALKHOME" ]; then
+	echo "The environment variable LOGTALKHOME should be defined first, pointing"
+	echo "to your Logtalk installation directory!"
+	echo "Trying the default locations for the Logtalk installation..."
+	if [ -d "/usr/local/share/logtalk" ]; then
+		LOGTALKHOME=/usr/local/share/logtalk
+		echo "... using Logtalk installation found at /usr/local/share/logtalk"
+	elif [ -d "/usr/share/logtalk" ]; then
+		LOGTALKHOME=/usr/share/logtalk
+		echo "... using Logtalk installation found at /usr/share/logtalk"
+	elif [ -d "/opt/local/share/logtalk" ]; then
+		LOGTALKHOME=/opt/local/share/logtalk
+		echo "... using Logtalk installation found at /opt/local/share/logtalk"
+	elif [ -d "/opt/share/logtalk" ]; then
+		LOGTALKHOME=/opt/share/logtalk
+		echo "... using Logtalk installation found at /opt/share/logtalk"
+	else
+		echo "... unable to locate Logtalk installation directory!"
+		echo
+		exit 1
+	fi
+	echo
+elif ! [ -d "$LOGTALKHOME" ]; then
+	echo "The environment variable LOGTALKHOME points to a non-existing directory!"
+	echo "Its current value is: $LOGTALKHOME"
+	echo "The variable must be set to your Logtalk installation directory!"
+	echo
+	exit 1
+fi
+export LOGTALKHOME
+
+if ! [ "$LOGTALKUSER" ]; then
+	echo "The environment variable LOGTALKUSER should be defined first, pointing"
+	echo "to your Logtalk user directory!"
+	echo "Trying the default location for the Logtalk user directory..."
+	echo
+	export LOGTALKUSER=$HOME/logtalk
+fi
+
+if [ -d "$LOGTALKUSER" ]; then
+	if ! [ -a "$LOGTALKUSER/VERSION.txt" ]; then
+		echo "Logtalk user directory at $LOGTALKUSER is outdated!"
+		echo "Creating an up-to-date Logtalk user directory..."
+		cplgtdirs
+	else
+		current=`cat $LOGTALKUSER/VERSION.txt | sed 's/\.//g'`
+		if [ $current -lt 2313 ]; then
+			echo "Logtalk user directory at $LOGTALKUSER is outdated!"
+			echo "Creating an up-to-date Logtalk user directory..."
+			cplgtdirs
+		fi
+	fi
+else
+	echo "Cannot find \$LOGTALKUSER directory! Creating a new Logtalk user directory"
+	echo "by running the \"cplgtdirs\" shell script:"
+	cplgtdirs
+fi
+echo
 
 if sicstus -f --goal "halt." 2>&1 | grep "SICStus 4" 2>&1 >/dev/null; then
 	exec sicstus -l "$LOGTALKHOME/integration/logtalk_sicstus4.pl" "$@"
