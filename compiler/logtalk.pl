3fd2930dfd3b614033cd41c7fc566c80bd7e1901
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-03-27 23:23:57 +0000

    Corrected typo.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4173 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -13116,7 +13116,7 @@ current_logtalk_flag(version, version(2, 31, 5)).
 '$lgt_mt_threaded_call_cancel'([Id| Ids]) :-
 	(	catch(thread_peek_message(Id, '$lgt_master'), _, fail) ->
 		catch(thread_send_message(Id, '$lgt_result'(_, terminate)), _, true)
-	;	catch(thread_signal(Id, (mutex_unlock_all, throw(abort)), _, true)
+	;	catch(thread_signal(Id, (mutex_unlock_all, throw(abort))), _, true)
 	),
 	'$lgt_mt_threaded_call_cancel'(Ids).
 
