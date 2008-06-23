c065f99588951b4cf2b721b218d3e52988a13357
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-06-23 18:32:13 +0000

    Updated the SWI-Prolog config file notes about possible compatibility issues with the default setting of the proprietary "iso" Prolog flag to "true". Removed redundant setting of the "iso" flag to "true" from the Logtalk/SWI-Prolog integration file.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4322 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/logtalk_comp_swi.pl
+++ integration/logtalk_comp_swi.pl
@@ -9,14 +9,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-:- set_prolog_flag(iso, true).
 :- set_prolog_flag(generate_debug_info, false).
 :- system_module.
 :- op(600, xfy, ::).
 :- op(600,  fy, ::).
 :- op(600,  fy, ^^).
-:- op(200, fy, +).
-:- op(200, fy, ?).
-:- op(200, fy, @).
-:- op(200, fy, -).
+:- op(200,  fy,  +).
+:- op(200,  fy,  ?).
+:- op(200,  fy,  @).
+:- op(200,  fy,  -).
 :- include('../compiler/logtalk.pl').
