45e531b0fb4507bdb5b57d8718e7fb7cabec2ca5
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-07-05 08:59:51 +0000

    Updated the "k6.config" config file for the K-Prolog 6.0.3 release. Renamed the K-Prolog 5.1.x config file from "k.config" to "k5.config" to avoid being mistaken for the K-Prolog 6.0.x config file.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4338 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/logtalk_plc.pl
+++ integration/logtalk_plc.pl
@@ -9,6 +9,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-:- consult('$LOGTALKUSER/configs/k.config').
+:- consult('$LOGTALKUSER/configs/k5.config').
 :- consult('$LOGTALKHOME/compiler/logtalk.pl').
 :- consult('$LOGTALKUSER/libpaths/libpaths.pl').
