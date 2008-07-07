81654354d24a90ca9551e2ed6062bfc87fd90212
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-07-07 15:56:18 +0000

    Minor documentation improvements. Corrected syntax errors on the shell scripts due to the updating of the licensing condition comments.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4351 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- xml/lgt2txt.sh
+++ xml/lgt2txt.sh
@@ -5,16 +5,16 @@
 ## Release 2.32.1
 ##
 ## Copyright (c) 1998-2008 Paulo Moura. All Rights Reserved.
-
-Logtalk is free software. You can redistribute it and/or modify
-it under the terms of the Artistic License 2.0 as published by 
-the The Perl Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-Artistic License 2.0 for more details. A copy of the license is 
-provided in the "LICENSE.txt" file.
+## 
+## Logtalk is free software. You can redistribute it and/or modify
+## it under the terms of the Artistic License 2.0 as published by 
+## the The Perl Foundation.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## Artistic License 2.0 for more details. A copy of the license is 
+## provided in the "LICENSE.txt" file.
 ## ================================================================
 
 if ! [ "$LOGTALKHOME" ]; then
