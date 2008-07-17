23f7490ca2101442adc294e6fa295c8611319e86
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-07-17 07:34:09 +0000

    Added a new example, "logging", of using a category to define a set of predicates that handle a dynamic predicate in the context of "this" (i.e. in the context of the objects importing the category).
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4385 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- libpaths/libpaths.pl
+++ libpaths/libpaths.pl
@@ -48,6 +48,7 @@
 	 assertz(logtalk_library_path(integration, threads('integration/'))),
 	 assertz(logtalk_library_path(lo_planner, examples('lo/planner/'))),
 	 assertz(logtalk_library_path(lo_travellers, examples('lo/travellers/'))),
+	 assertz(logtalk_library_path(logging, examples('logging/'))),
 	 assertz(logtalk_library_path(logic, examples('logic/'))),
 	 assertz(logtalk_library_path(lpa_faults, examples('lpa/faults/'))),
 	 assertz(logtalk_library_path(lpa_timetables, examples('lpa/timetables/'))),
