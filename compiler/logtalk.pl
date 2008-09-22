96c1bfcc90975dfc32aff805161c6e7cf6dd319d
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-09-22 19:04:00 +0000

    Added a syntax construct for easy access to parametric object proxies represented as Prolog facts when sending a message ({Proxy}::Message). Updated the "proxies" example to illustrate this new functionality.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4471 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -7884,7 +7884,9 @@ current_logtalk_flag(version, version(2, 33, 1)).
 
 % convenient access to parametric object proxies
 
-'$lgt_tr_msg'(Pred, {Proxy}, (call(Proxy), TPred), This) :-
+'$lgt_tr_msg'(Pred, Obj, (call(Proxy), TPred), This) :-
+    nonvar(Obj),
+    Obj = {Proxy},
     !,
     '$lgt_tr_msg'(Pred, Proxy, TPred, This).
 
