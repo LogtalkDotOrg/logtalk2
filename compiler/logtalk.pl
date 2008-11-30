e82b47681e4ea099cb37c6fbf60afea695eaf005
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2008-11-30 18:03:56 +0000

    Updated the MacOS X installer postflight script to open the Logtalk installation folder and the "README.txt" file.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4624 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -1901,7 +1901,7 @@ current_logtalk_flag(Flag, Value) :-
 	'$lgt_default_flag'(Flag, Value),
 	\+ '$lgt_current_flag_'(Flag, _).
 
-current_logtalk_flag(version, version(2, 34, 0)).
+current_logtalk_flag(version, version(2, 34, 1)).
 
 
 
