f0ff1b626f00ecb5e5093ccef7b13becc8ed4e23
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2009-03-12 17:57:24 +0000

    Updated the Logtalk compiler and runtime and all the config files to use the call/N predicates instead of the call_with_args/N predicates.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4731 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/sicstuslgt.sh
+++ integration/sicstuslgt.sh
@@ -60,7 +60,7 @@ if [ -d "$LOGTALKUSER" ]; then
 		cplgtdirs
 	else
 		current=`cat $LOGTALKUSER/VERSION.txt | sed 's/\.//g'`
-		if [ $current -lt 2351 ]; then
+		if [ $current -lt 2352 ]; then
 			echo "Logtalk user directory at $LOGTALKUSER is outdated!"
 			echo "Creating an up-to-date Logtalk user directory..."
 			cplgtdirs
