e67877b5967246b334f3f5e4c6b317c8c8e352ca
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2009-03-14 21:12:10 +0000

    Extended the experimental support for defining Logtalk startup directory specific settings to Qu-Prolog.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4745 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/logtalk_qp.pl
+++ integration/logtalk_qp.pl
@@ -14,6 +14,11 @@
 
 :- 	fcompile('$LOGTALKUSER/configs/qu.pl', [assemble_only(true)]),
 	load('$LOGTALKUSER/configs/qu.qo'),
+	(	access('settings.pl', 4, 0) ->
+		fcompile('settings.pl', [assemble_only(true)]),
+		load('settings.qo')
+	;	true
+	),
 	os(system('ln -sf $LOGTALKHOME/compiler/logtalk.pl $LOGTALKUSER/.logtalk.pl')),
 	fcompile('$LOGTALKUSER/.logtalk.pl', [assemble_only(true), object_file('$LOGTALKUSER/.logtalk.qo'), string_table(256)]),
 	load('$LOGTALKUSER/.logtalk.qo'),
