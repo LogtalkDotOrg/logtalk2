f321d794c4b8ef07523944b8e8cca4ba142dd721
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2009-03-20 11:08:34 +0000

    Updated the SWI-Prolog POSIX integration scripts and Windows shortcut to use the command-line option "-s" instead of "-f" in order to allow loading of SWI-Prolog initialization files.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4758 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/swilgt.sh
+++ integration/swilgt.sh
@@ -74,11 +74,11 @@ fi
 echo
 
 if pl -t halt 2>&1 | grep "SWI-Prolog"; then
-	exec pl -f "$LOGTALKHOME/integration/logtalk_swi.pl" "$@"
+	exec pl -s "$LOGTALKHOME/integration/logtalk_swi.pl" "$@"
 elif swipl -t halt 2>&1 | grep "SWI-Prolog"; then
-	exec swipl -f "$LOGTALKHOME/integration/logtalk_swi.pl" "$@"
+	exec swipl -s "$LOGTALKHOME/integration/logtalk_swi.pl" "$@"
 else case $( uname -s ) in
-	Darwin	) exec swipl -f "$LOGTALKHOME/integration/logtalk_swi.pl" "$@";;
-	*		) exec pl -f "$LOGTALKHOME/integration/logtalk_swi.pl" "$@";;
+	Darwin	) exec swipl -s "$LOGTALKHOME/integration/logtalk_swi.pl" "$@";;
+	*		) exec pl -s "$LOGTALKHOME/integration/logtalk_swi.pl" "$@";;
 esac
 fi
