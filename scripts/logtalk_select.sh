34bc58879d45cb7f338993242e5fe422bf1f1983
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2009-05-25 16:52:31 +0000

    Added an experimental "logtalk_select.sh" POSIX shell script for switching between installed Logtalk versions.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4965 ac9c905b-388a-4ddf-9b6a-4063a022bc57

---
+++ scripts/logtalk_select.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+## ================================================================
+## Logtalk - Open source object-oriented logic programming language
+## Release 2.37.1
+## 
+## Copyright (c) 1998-2009 Paulo Moura.        All Rights Reserved.
+## Logtalk is free software.  You can redistribute it and/or modify
+## it under the terms of the "Artistic License 2.0" as published by 
+## The Perl Foundation. Consult the "LICENSE.txt" file for details.
+## ================================================================
+
+
+print_version() {
+	echo "`basename $0` 0.1"
+	exit 1
+}
+
+
+list_versions() {
+    echo -n "Available versions: "
+	for file in $(ls -d "$LOGTALKHOME/../lgt*"); do
+		echo -n "`basename $file` "
+	done
+	echo
+	exit 1
+}
+
+
+show_selected() {
+    echo -n "Current version: "
+    if [ -e "$LOGTALKHOME" ]; then
+		readlink "$LOGTALKHOME"
+    else
+        echo "none"
+    fi
+	exit 1
+}
+
+
+usage_help() {
+	echo 
+	echo "This script allows switching between installed Logtalk versions"
+	echo
+	echo "Usage:"
+	echo "  `basename $0` [-vlsh] version"
+	echo
+	echo "Optional arguments:"
+	echo "  -v print version of `basename $0`"
+	echo "  -l list available versions"
+	echo "  -s show the currently selected version"
+	echo "  -h help"
+	echo
+	exit 1
+}
+
+
+valid_version() {
+    for version in $(ls -d "$LOGTALKHOME/../lgt*"); do
+        if [ "${1}" == "`basename ${version}`" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+
+switch_version() {
+	valid_version ${1}
+	if [ 0 != ${?} ]; then
+    	echo "Invalid version: \"${1}\"!"
+    	exit 1
+	else
+		cd "$LOGTALKHOME/.."
+		rm logtalk
+		ln -sf ${1} logtalk
+		exit 0
+	fi
+}
+
+
+while getopts "vlsh" Option
+do
+	case $Option in
+		v) print_version;;
+		l) list_versions;;
+		s) show_selected;;
+		h) usage_help;;
+		*) usage_help;;
+	esac
+done
+
+
+switch_version ${1}
+error=${?}
+if [ 0 != ${error} ]; then
+    echo "An error occurred when activating version \"${version}\"!"
+    exit 1
+fi
+exit 0
