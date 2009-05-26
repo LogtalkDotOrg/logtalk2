efada4c30efc12059491b9a49f1917b046215f52
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2009-05-26 14:34:19 +0000

    Updated the experimental "logtalk_select.sh" POSIX shell script for switching between installed Logtalk versions to require version 2.36.0 or later.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@4972 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/logtalk_select.sh
+++ scripts/logtalk_select.sh
@@ -98,6 +98,38 @@ do
 done
 
 
+if ! [ "$LOGTALKHOME" ]; then
+	echo "The environment variable LOGTALKHOME should be defined first, pointing"
+	echo "to your Logtalk installation directory!"
+	echo "Trying the default locations for the Logtalk installation..."
+	if [ -d "/usr/local/share/logtalk" ]; then
+		LOGTALKHOME=/usr/local/share/logtalk
+		echo "... using Logtalk installation found at /usr/local/share/logtalk"
+	elif [ -d "/usr/share/logtalk" ]; then
+		LOGTALKHOME=/usr/share/logtalk
+		echo "... using Logtalk installation found at /usr/share/logtalk"
+	elif [ -d "/opt/local/share/logtalk" ]; then
+		LOGTALKHOME=/opt/local/share/logtalk
+		echo "... using Logtalk installation found at /opt/local/share/logtalk"
+	elif [ -d "/opt/share/logtalk" ]; then
+		LOGTALKHOME=/opt/share/logtalk
+		echo "... using Logtalk installation found at /opt/share/logtalk"
+	else
+		echo "... unable to locate Logtalk installation directory!"
+		echo
+		exit 1
+	fi
+	echo
+elif ! [ -d "$LOGTALKHOME" ]; then
+	echo "The environment variable LOGTALKHOME points to a non-existing directory!"
+	echo "Its current value is: $LOGTALKHOME"
+	echo "The variable must be set to your Logtalk installation directory!"
+	echo
+	exit 1
+fi
+export LOGTALKHOME
+
+
 if [ "$1" == "" ]; then
 	usage_help
 else
