80cea0c91a7ff33d494246d5328411430ed9ee2d
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2010-02-17 14:30:18 +0000

    Updated version number to 2.39.0 due to semantic changes in the forthcoming stable release.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@5283 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -2,7 +2,7 @@
 
 ## ================================================================
 ## Logtalk - Open source object-oriented logic programming language
-## Release 2.38.3
+## Release 2.39.0
 ## 
 ## Copyright (c) 1998-2010 Paulo Moura.        All Rights Reserved.
 ## Logtalk is free software.  You can redistribute it and/or modify
@@ -12,23 +12,23 @@
 
 dir=`PWD`
 
-svn export http://svn.logtalk.org/logtalk/trunk lgt2383
+svn export http://svn.logtalk.org/logtalk/trunk lgt2390
 
-cd lgt2383
+cd lgt2390
 chmod a+x scripts/cleandist.sh
 scripts/cleandist.sh
 
 cd ..
-cp -R lgt2383/manuals man2383
-tar -czf man2383.tgz man2383
-tar -cjf lgt2383.tar.bz2 lgt2383
+cp -R lgt2390/manuals man2390
+tar -czf man2390.tgz man2390
+tar -cjf lgt2390.tar.bz2 lgt2390
 
 mkdir -p debian/usr/bin
 mkdir -p debian/usr/share/doc/logtalk
 mkdir -p debian/usr/share/doc-base
 mkdir -p debian/usr/share/menu
 mkdir -p debian/DEBIAN
-cd lgt2383/scripts
+cd lgt2390/scripts
 ./install.sh $dir/debian/usr
 rm -rf $dir/debian/usr/share/mime
 cp debian/logtalk.doc-base $dir/debian/usr/share/doc-base/logtalk-docs
@@ -47,29 +47,29 @@ cp debian/postinst $dir/debian/DEBIAN
 cp debian/prerm $dir/debian/DEBIAN
 cp debian/postrm $dir/debian/DEBIAN
 cd $dir
-dpkg-deb --build debian logtalk_2.38.3-1_all.deb
+dpkg-deb --build debian logtalk_2.39.0-1_all.deb
 
-md5="`md5 -q lgt2383.tar.bz2`"
+md5="`md5 -q lgt2390.tar.bz2`"
 sudo mkdir -p /opt/local/var/macports/distfiles/logtalk
-sudo cp -f lgt2383.tar.bz2 /opt/local/var/macports/distfiles/logtalk/lgt2383.tar.bz2
+sudo cp -f lgt2390.tar.bz2 /opt/local/var/macports/distfiles/logtalk/lgt2390.tar.bz2
 cd /opt/local/var/macports/sources/rsync.macports.org/release/ports/lang/logtalk/
 sudo mv -f Portfile Portfile.old
-sudo cp $dir/lgt2383/scripts/macosx/Portfile .
-sudo sed -e 's/^version.*/version 2.38.3/' -i '' Portfile
+sudo cp $dir/lgt2390/scripts/macosx/Portfile .
+sudo sed -e 's/^version.*/version 2.39.0/' -i '' Portfile
 sudo sed -e "s/^checksums.*/checksums md5 $md5/" -i '' Portfile
 sudo port clean --archive logtalk
 sudo port destroot logtalk
 sudo port pkg logtalk
-cp -R work/logtalk-2.38.3.pkg $dir
+cp -R work/logtalk-2.39.0.pkg $dir
 sudo port clean logtalk
 
 cd $dir
-mkdir manpdf2383
-cd man2383/userman
+mkdir manpdf2390
+cd man2390/userman
 ./userman.sh
-mv userman.pdf ../../manpdf2383/lgtuserman2383.pdf
+mv userman.pdf ../../manpdf2390/lgtuserman2390.pdf
 cd ../refman
 ./refman.sh
-mv refman.pdf ../../manpdf2383/lgtrefman2383.pdf
+mv refman.pdf ../../manpdf2390/lgtrefman2390.pdf
 cd ../..
-tar -czf manpdf2383.tgz manpdf2383
+tar -czf manpdf2390.tgz manpdf2390
--- xml/lgtxml.xsl
+++ xml/lgtxml.xsl
@@ -16,7 +16,7 @@
 <!--
 ================================================================
 Logtalk - Open source object-oriented logic programming language
-Release 2.38.3
+Release 2.39.0
 
 Copyright (c) 1998-2010 Paulo Moura.        All Rights Reserved.
 Logtalk is free software.  You can redistribute it and/or modify
