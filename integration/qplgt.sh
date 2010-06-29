158692af9efd46807373753a46c51bcad1cdf7e3
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2010-06-29 01:09:31 +0000

    Updated the integration scripts in order to require an up-to-date Logtalk user folder (due to the improved examples in the 2.40.1 release).
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@5661 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- integration/qplgt.sh
+++ integration/qplgt.sh
@@ -60,7 +60,7 @@ if [ -d "$LOGTALKUSER" ]; then
 		logtalk_user_setup
 	else
 		current=`cat $LOGTALKUSER/VERSION.txt | sed 's/\.//g'`
-		if [ $current -lt 2400 ]; then
+		if [ $current -lt 2401 ]; then
 			echo "Logtalk user directory at $LOGTALKUSER is outdated!"
 			echo "Creating an up-to-date Logtalk user directory..."
 			logtalk_user_setup
