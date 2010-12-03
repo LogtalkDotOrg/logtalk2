4c8a6baf42bf744cde982be05cfa319221def812
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2010-12-03 00:28:58 +0000

    Update the version number to 2.42.1 in preparation for the next stable release.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@5951 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- compiler/logtalk.pl
+++ compiler/logtalk.pl
@@ -2,7 +2,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %  Logtalk - Open source object-oriented logic programming language
-%  Release 2.42.0
+%  Release 2.42.1
 %  
 %  Copyright (c) 1998-2010 Paulo Moura.        All Rights Reserved.
 %  Logtalk is free software.  You can redistribute it and/or modify
@@ -2151,7 +2151,7 @@ current_logtalk_flag(Flag, Value) :-
 current_logtalk_flag(Flag, Value) :-
 	'$lgt_prolog_feature'(Flag, Value).
 
-current_logtalk_flag(version, version(2, 42, 0)).
+current_logtalk_flag(version, version(2, 42, 1)).
 
 
 
