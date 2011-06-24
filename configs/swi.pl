390b256df74033d3d8ccb5e21a675ad3258c3438
pmoura <pmoura@ac9c905b-388a-4ddf-9b6a-4063a022bc57>
2011-06-24 01:52:06 +0000

    Updated the SWI-Prolog config file with a better workaround for the "public" operator clash.
    git-svn-id: http://svn.logtalk.org/logtalk/trunk@6302 ac9c905b-388a-4ddf-9b6a-4063a022bc57

--- configs/swi.pl
+++ configs/swi.pl
@@ -11,7 +11,7 @@
 %
 %  configuration file for SWI Prolog 5.8.0 and later versions
 %
-%  last updated: June 5, 2011
+%  last updated: June 23, 2011
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -519,12 +519,18 @@ message_hook(discontiguous(_), _, _) :-		% SWI-Prolog discontiguous predicate
 
 % '$lgt_read_term'(@stream, -term, +list, -position)
 
-:- if(current_op(_, _, (public))).	% workaround SWI-Prolog 5.11.9 operator clash
+:- if(current_op(_, _, (public))).
+	% workaround SWI-Prolog 5.11.9 operator clash
 	'$lgt_read_term'(Stream, Term, Options, LineBegin-LineEnd) :-
-		current_op(Priority, Specifier, (public)), !,
-		op(0, Specifier, (public)),
-		read_term(Stream, Term, [term_position(PositionBegin)| Options]),
-		op(Priority, Specifier, (public)),
+		(	current_op(Priority, Specifier, (public)),
+			\+ '$lgt_pp_module_'(_) ->
+			setup_call_cleanup(
+				op(0, Specifier, (public)),
+				read_term(Stream, Term, [term_position(PositionBegin)| Options]),
+				op(Priority, Specifier, (public))
+			)
+		;	read_term(Stream, Term, [term_position(PositionBegin)| Options])
+		),
 		stream_position_data(line_count, PositionBegin, LineBegin),
 		stream_property(Stream, position(PositionEnd)),
 		stream_position_data(line_count, PositionEnd, LineEnd).
